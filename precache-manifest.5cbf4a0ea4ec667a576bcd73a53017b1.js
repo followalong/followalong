73c1b2ee973c909cec75a981224c4303929d9af0
FollowAlong <followalong@protonmail.com>
2021-01-12 18:36:48 -0400

    Publish 1.2.3: Pull Services and Help into the sidebar. Internal updates to Vue 3!

---
+++ precache-manifest.5cbf4a0ea4ec667a576bcd73a53017b1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58fc908240a6483f6807d9d1585ba858",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "5f926fe6b6476b67b08b",
+    "url": "/app.css"
+  },
+  {
+    "revision": "5f926fe6b6476b67b08b",
+    "url": "/app.js"
+  },
+  {
+    "revision": "6269294fe0e1884d7e50",
+    "url": "/chunk-vendors.js"
+  },
+  {
+    "revision": "d34c5e041bbc02a83861a2297b032fb0",
+    "url": "/img/apple-touch-icon-blue.png"
+  },
+  {
+    "revision": "5dfb2cd60d731121288203861cf56c2b",
+    "url": "/img/apple-touch-icon.png"
+  },
+  {
+    "revision": "e25efedb6a4a8e2115b51b9e7511a89a",
+    "url": "/img/favicon-white.png"
+  },
+  {
+    "revision": "b9a735350dfb111356e1606717cd2b69",
+    "url": "/img/favicon-white.svg"
+  },
+  {
+    "revision": "8268504d5bd20cc686dcfb9311f395e9",
+    "url": "/img/favicon.svg"
+  },
+  {
+    "revision": "fc34d054540c759d545d77727f03bf39",
+    "url": "/img/logo-white.svg"
+  },
+  {
+    "revision": "1453cf1dc80fac9332b6ddfa2f76d57b",
+    "url": "/img/logo.svg"
+  },
+  {
+    "revision": "1402e9eb85ea9c6d368dbb2be9974cb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3d77cfb806d6b28c253ec7540566ae3",
+    "url": "/js/aws-sdk-2.444.0.min.js"
+  },
+  {
+    "revision": "25cf41f6582aa7dc4f499ebbd7b6ec43",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "953f703f8af8e25862cce4b76df621c0",
+    "url": "/sw.js"
+  }
+]);
