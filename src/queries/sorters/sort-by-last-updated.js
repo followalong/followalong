48a2583c64fcf2c671f5afe590ed241490989cbf
FollowAlong <followalong@protonmail.com>
2021-10-28 13:21:16 -0300

    For perpetual, check the least recently updated feed; not the most recently updated

--- src/queries/sorters/sort-by-last-updated.js
+++ src/queries/sorters/sort-by-last-updated.js
@@ -1,5 +1,5 @@
 export default (a, b) => {
-  if (a._updatedAt < b._updatedAt) return -1
-  if (a._updatedAt > b._updatedAt) return 1
+  if (a.updatedAt < b.updatedAt) return -1
+  if (a.updatedAt > b.updatedAt) return 1
   return 0
 }
