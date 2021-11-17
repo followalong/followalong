232811bfb6a5d6bf34e40e93a35c6cc2d8173cfc
FollowAlong <followalong@protonmail.com>
2021-11-16 21:43:58 -0400

    Remove unused files

--- use-cases/app/see-latest-items.spec.js
+++
@@ -1,9 +0,0 @@
-import { mountApp } from '../helper.js'
-
-describe('App: See latest items', () => {
-  it('see ', async () => {
-    const app = await mountApp()
-
-    expect(app.find('h1').text()).toContain('What\'s New?')
-  })
-})
