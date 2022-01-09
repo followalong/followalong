46974fada68d3444bb0899067326aec141a9fa04
FollowAlong <followalong@protonmail.com>
2022-01-08 20:11:53 -0400

    Update fetcher

--- src/commands/index.js
+++ src/commands/index.js
@@ -349,11 +349,11 @@ class Commands {
       return
     }
 
-    const DELAY_BETWEEN_FETCHES = 2 * 60 * 1000
+    const DELAY_BETWEEN_FETCHES = 3 * 1000
     const done = () => setTimeout(() => this.fetchNextFeedPerpetually(identity), DELAY_BETWEEN_FETCHES)
     // const maxFeedsPerFetch = 1 // Math.round(this.queries.feedsForIdentity(identity).length / 3)
     // const feeds = this.queries.findOutdatedFeeds(identity).slice(0, maxFeedsPerFetch)
-    const feeds = this.queries.findOutdatedFeeds(identity)
+    const feeds = [this.queries.findOutdatedFeeds(identity)[0]]
 
     if (!feeds.length) return done()
 
