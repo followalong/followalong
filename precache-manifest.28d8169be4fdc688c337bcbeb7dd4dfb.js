9fb2f172e60868cddc6ed0cec9372fa25c8ef8ae
FollowAlong <followalong@protonmail.com>
2022-10-22 11:05:31 -0300

    gh-pages

---
+++ precache-manifest.28d8169be4fdc688c337bcbeb7dd4dfb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58fc908240a6483f6807d9d1585ba858",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "e4adfb4c9f0570d45ffb",
+    "url": "/app.css"
+  },
+  {
+    "revision": "e4adfb4c9f0570d45ffb",
+    "url": "/app.js"
+  },
+  {
+    "revision": "d424ebfdf139229a3736",
+    "url": "/chunk-vendors.js"
+  },
+  {
+    "revision": "d34c5e041bbc02a83861a2297b032fb0",
+    "url": "/img/apple-touch-icon-blue.png"
+  },
+  {
+    "revision": "5dfb2cd60d731121288203861cf56c2b",
+    "url": "/img/apple-touch-icon.png"
+  },
+  {
+    "revision": "e25efedb6a4a8e2115b51b9e7511a89a",
+    "url": "/img/favicon-white.png"
+  },
+  {
+    "revision": "b9a735350dfb111356e1606717cd2b69",
+    "url": "/img/favicon-white.svg"
+  },
+  {
+    "revision": "8268504d5bd20cc686dcfb9311f395e9",
+    "url": "/img/favicon.svg"
+  },
+  {
+    "revision": "4d21eac7b5796fe283b6cd1c01fb9142",
+    "url": "/img/logo-icon.svg"
+  },
+  {
+    "revision": "5f837bafe97e6331ac8317a24d16c352",
+    "url": "/img/logo-white.svg"
+  },
+  {
+    "revision": "a7996ba09032627fee2575935cdf0537",
+    "url": "/img/logo-word.svg"
+  },
+  {
+    "revision": "1453cf1dc80fac9332b6ddfa2f76d57b",
+    "url": "/img/logo.svg"
+  },
+  {
+    "revision": "03e8b33cbc5efed83fc0dbe15d1b7570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e751d5301ab8fb2b6636d3232adfd666",
+    "url": "/js/aws-sdk-2.444.0.min.js"
+  },
+  {
+    "revision": "25cf41f6582aa7dc4f499ebbd7b6ec43",
+    "url": "/manifest.json"
+  }
+]);
